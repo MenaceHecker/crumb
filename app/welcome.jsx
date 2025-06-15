a3ac46be1b7f28715a1071fb69daf1e12bf539d2
MenaceHecker <thedynamic2508@gmail.com>
2025-06-14 22:01:55 -0400

    Working on welcome page

---
+++ app/welcome.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Welcome = () => {
+  return (
+    <ScreenWrapper>
+      <Text>Welcome</Text>
+    </ScreenWrapper>
+  )
+}
+
+export default Welcome
+
+const styles = StyleSheet.create({})
