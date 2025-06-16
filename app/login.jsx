848e1f8f713345beb0a09c7e6e7c6ac3865827c4
MenaceHecker <thedynamic2508@gmail.com>
2025-06-16 19:54:46 -0400

    Added styles to login

--- app/login.jsx
+++ app/login.jsx
@@ -22,4 +22,34 @@ const Login = () => {
 
 export default Login
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        paddingHorizontal: wp(5),
+        gap: 45,
+    },
+    welcomeText: {
+        fontSize: hp(4),
+        fontWeight: theme.fonts.bold,
+        color: theme.colors.text,
+    },
+    form: {
+        gap: 25,
+    },
+    forgotPassword: {
+        textAlign: 'right',
+        color: theme.colors.text,
+        fontWeight: theme.fonts.semibold,
+    },
+    footer: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        gap: 5,
+    },
+    footerText: {
+        textAlign: 'center',
+        color: theme.colors.text,
+        fontSize: hp(1.6),
+    }
+})
