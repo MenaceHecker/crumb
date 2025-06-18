592efc5295fd47988dbf0d2fe0b15052d72b24a4
MenaceHecker <thedynamic2508@gmail.com>
2025-06-18 12:37:26 -0400

    setting up Auth in the home file

--- app/(main)/home.jsx
+++ app/(main)/home.jsx
@@ -1,11 +1,24 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Alert, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import ButtonGen from '../../components/ButtonGen'
+import { useAuth } from '../contexts/AuthContext'
+
 
 const Home = () => {
+    const {setAuth} = useAuth();
+    const onLogout = async () => {
+        setAuth(null);
+        const {error} = await supabase.auth.signOut();
+        if (error) {
+            Alert.alert('Sign Out', error.message);
+        }
+    }
+
   return (
-    <View>
+    <ScreenWrapper>
       <Text>Home</Text>
-    </View>
+      <ButtonGen title='Logout' onPress={onLogout} />
+    </ScreenWrapper>
   )
 }
 
