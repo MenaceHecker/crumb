4d1c5aad1f1ac27e2ebe89116d953c3a2050d570
MenaceHecker <thedynamic2508@gmail.com>
2025-06-18 11:26:38 -0400

    Gotta fix the error

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,10 +1,25 @@
 import { View, Text, Button } from 'react-native'
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Stack, useRouter } from 'expo-router'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
+import { AuthProvider } from './contexts/AuthContext'
 
 
 const _layout = () => {
+  return (
+    <AuthProvider>
+      <MainLayout />
+    </AuthProvider>
+  )
+}
+
+
+const MainLayout = () => {
+  const {setAuth} = useAuth();
+  useEffect(() => {
+    supabase.auth.onAuthStateChange((_event, session) => {
+      console.log ('session user: ', session?.user);
+    })
   return (
     <Stack
     screenOptions={{ headerShown: false}}
@@ -13,3 +28,5 @@ const _layout = () => {
 }
 
 export default _layout
+
+//There is still error in the code
