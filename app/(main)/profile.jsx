5f06836c78ba90e574e2ae54a2d429f1d204e337
MenaceHecker <thedynamic2508@gmail.com>
2025-06-19 22:21:34 -0400

    Now working on profile

--- app/(main)/profile.jsx
+++ app/(main)/profile.jsx
@@ -1,16 +1,17 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-import ScreenWrapper from '../../components/ScreenWrapper'
-import { useAuth } from '../../contexts/AuthContext'
-import {useRouter} from 'expo-router'
+import ScreenWrapper from '../components/ScreenWrapper'
+import { useRouter } from 'expo-router'
+import {useAuth} from '../hooks/useAuth'
+
 
 const Profile = () => {
-  const {user, setAuth} = useAuth();
-
+  const router = useRouter();
+  const { user, setAuth } = useAuth();
   return (
     <ScreenWrapper>
 
-
+      <Text>Profile</Text>
     </ScreenWrapper>>
   )
 }
