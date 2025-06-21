59ec7ab2493570ef330ac77056a746ccd664580f
MenaceHecker <thedynamic2508@gmail.com>
2025-06-21 14:51:09 -0400

    Still working on Edit Profile, now added a text field

--- app/(main)/editProfile.jsx
+++ app/(main)/editProfile.jsx
@@ -1,13 +1,46 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Pressable, ScrollView, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import ScreenWrapper from '../../components/ScreenWrapper'
-import { hp } from '../../helpers/common'
+import { hp, wp } from '../../helpers/common'
 import { theme } from '../../constants/theme'
+import Header from '../../components/Header'
+import { Image } from 'expo-image'
+import { useAuth } from '../../contexts/AuthContext'
+import { getUserImageSrc } from '../../services/imageService'
+import Icon from '../../assets/icons'
+import Input from '../../components/Input'
 
 const EditProfile = () => {
+    const {user} = useAuth();
+    const onPickImage = {
+
+    }
+    let imageSource = getUserImageSrc(user.image);
   return (
-    <ScreenWrapper>
-      <Text>EditProfile</Text>
+    <ScreenWrapper bg="white">
+      <View style={styles.container}>
+        <ScrollView style ={{flex:1}}>
+            <Header title = "Edit Profile" />
+            {/* Here goes a form to edit */}
+            <View style={styles.form}>
+                <View style={styles.avatarContainer}>
+                    <Image source={imageSource} style={styles.avatar} />
+                    <Pressable style={styles.cameraIcon} onPress={onPickImage}>
+                        <Icon name="camera" size = {20} strokeWidth={2.5}/>
+                    </Pressable>
+                </View>
+                <Text style={{fontSize: hp(1.5), color: theme.colors.text}}>
+                    Wut are your profile about
+                </Text>
+                <Input
+                icon ={<Icon name={"user"}/>}
+                placeholder="Your name please?"
+                value = {null}
+                onChangeText = {value => {}}
+                />
+            </View>
+        </ScrollView>
+      </View>
     </ScreenWrapper>
   )
 }
