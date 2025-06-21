dad4d87b1cf1f4ded95a792da4f1760c06726b7d
MenaceHecker <thedynamic2508@gmail.com>
2025-06-20 23:10:44 -0400

    Working on header of profile

--- app/(main)/profile.jsx
+++ app/(main)/profile.jsx
@@ -1,18 +1,28 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import ScreenWrapper from '../components/ScreenWrapper'
-import { useRouter } from 'expo-router'
+import { router, useRouter } from 'expo-router'
 import {useAuth} from '../hooks/useAuth'
+import Header from '../components/Header'
+import BackButton from '../../components/BackButton'
 
 
 const Profile = () => {
   const router = useRouter();
   const { user, setAuth } = useAuth();
   return (
-    <ScreenWrapper>
-
+    <ScreenWrapper bg='white'>
       <Text>Profile</Text>
-    </ScreenWrapper>>
+    </ScreenWrapper>
+  )
+}
+const userHeader = ({user, router}) => {
+  return (
+    <View style = {{flex:1, backgroundColor: 'white'}}>
+      <View>
+        <Header title="Profile" showBackButton = {true}/>
+      </View>
+    </View>
   )
 }
 
