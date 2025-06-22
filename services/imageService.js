35a901532140ad53052a2af2f2c9d86138df9414
MenaceHecker <thedynamic2508@gmail.com>
2025-06-21 21:15:03 -0400

    Working on uploading media

--- services/imageService.js
+++ services/imageService.js
@@ -1,3 +1,5 @@
+import { supabase } from '../lib/supabase';
+
 export const getUserImageSrc = imagePath => {
   if (!imagePath) { 
     return require('../assets/images/defaultUser2.png');
@@ -6,3 +8,38 @@ export const getUserImageSrc = imagePath => {
     return {uri: imagePath};
   }
 }
+export const uploadFile = async (folderName, fileUri, isImage = true) => {
+  try {
+      let fileName = getFilePath (folderName, isImage);
+      let filebBase64 = getFilePath(folderName, isImage);
+      const fileBase64 = await FileSystem.readAsStringAsync(fileUri, {
+            encoding : FileSystem.EncodingType.Base64
+      });
+      let imageData = decode(fileBase64); //This gives an array buffer
+      let {data, error} = await supabase
+      .storage
+      .from('uploads')
+      .upload(fileName, imageData, {
+            cacheControl: '3600',
+            upsert: false,
+            contentType: isImage? 'image/*' : 'video'
+      });
+      if(error) 
+      {
+        console.log('File Upload Error!', error);
+        return {success: false, msg: 'Could not upload the media'};
+      }
+      console.log('data', data);
+      return {success: true, data: data.path}
+      }
+
+  catch(error){
+    console.log('File upload error', error);
+    return {success: false, msg: 'Could not upload the media'};
+  }
+}
+export const getFilePath = (folderName, isImage) =>
+  {
+    // return /${folderName}/${new Date()).getTime()}${isImage?}
+    return `/${folderName}/${(new Date()).getTime()}${isImage? '.png' : '.mp4'}`;
+  }
