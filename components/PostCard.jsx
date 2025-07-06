0c7b055afb7018d8de3048b2cdd40747fb3daa5c
MenaceHecker <thedynamic2508@gmail.com>
2025-07-05 22:33:24 -0400

    Working on post card

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -1,7 +1,23 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import {hp, wp} from '../helpers/common'
+import { theme } from '../theme/theme'
 
-const PostCard = () => {
+const PostCard = ({
+    item,
+    currentUser,
+    router,
+    hasShadow = true,
+}) => {
+  const shadowStyles = {
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpactiy: 0.06,
+    shadowRadius: 6,
+    elevation: 1
+    }
   return (
     <View>
       <Text>PostCard</Text>
@@ -11,4 +27,9 @@ const PostCard = () => {
 
 export default PostCard
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  count: {
+    color: theme.colors.text,
+    fontSize: hp(1.8),
+  }
+})
