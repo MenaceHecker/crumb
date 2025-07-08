9c2ed8487d2ca4297c27f2d6bf64d6f688398714
MenaceHecker <thedynamic2508@gmail.com>
2025-07-08 16:22:10 -0400

    Working on post details useEffect

--- app/(main)/postDetails.jsx
+++ app/(main)/postDetails.jsx
@@ -1,6 +1,16 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { useLocalSearchParams } from 'expo-router';
+import { useEffect, useState } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
 const PostDetails = () => {
+    const {postId} = useLocalSearchParams();
+    const {post,setPost} = useState(null);
+    useEffect(() => {
+        getPostDetails();
+    }, []);
+    const postDetails = async () => {
+        //fetch post details from server here
+    }
   return (
     <View>
       <Text>PostDetails</Text>
