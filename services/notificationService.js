f03473b7532cbafef6306f9abc78287408575667
MenaceHecker <thedynamic2508@gmail.com>
2025-07-12 18:59:50 -0400

    fixing errors with notification service

--- services/notificationService.js
+++ services/notificationService.js
@@ -1,20 +1,20 @@
-export const createNotification = async (postLike) => {
+export const createNotification = async (notification) => {
     try {
         const {data,error} = await supabase 
-        .from('postLikes')
-        .insert(postLike)
+        .from('notifications')
+        .insert(notification)
         .select()
         .single();
 
         if(error){
-            console.log('postLike Error:', error);
-            return {success: false, msg: "Could not like the post"};
+            console.log('Notification Error:', error);
+            return {success: false, msg: "Something went wrong!"};
         }
         
         return {success: true, data: data};
     }
     catch(error) {
-        console.log('FetchPost Error:', error);
-        return {success: false, msg: "Could not like the post"};
+        console.log('Notification Error:', error);
+        return {success: false, msg: "Something went wrong!"};
     }
 }
