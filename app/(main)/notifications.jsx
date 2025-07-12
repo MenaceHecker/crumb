fd3ef6b0b5bffdf5adbd1686acc2113f2d881034
MenaceHecker <thedynamic2508@gmail.com>
2025-07-12 19:22:28 -0400

    Added styles for notifications

--- app/(main)/notifications.jsx
+++ app/(main)/notifications.jsx
@@ -1,11 +1,20 @@
 import { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { theme } from '../../constants/theme';
+import { hp, wp } from '../../helpers/common';
 
 const Notification = () => {
   const [notifications, setNotifications] = useState([]);
   useEffect(()=> {
       getNotifications();
   },[]);
+  const getNotifications = async () =>
+  {
+    let res = await fetchNotifications(UserActivation.id);
+    if(res.success){
+      setNotifications(res.data);
+    }
+  }
   return (
     <View>
       <Text>Notification</Text>
@@ -15,4 +24,19 @@ const Notification = () => {
 
 export default Notification
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: wp(4)
+  },
+  listStyle: {
+    paddingVertical: 20,
+    gap: 10
+  },
+  noData: {
+    fontSize: hp(1.8),
+    fontWeight: theme.fonts.medium,
+    color: theme.colors.text,
+    textAlign: 'center'
+  }
+})
