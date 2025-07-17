1a88dbd6d251e3888a3df8d060d089cfdb1cd85f
MenaceHecker <thedynamic2508@gmail.com>
2025-07-17 13:37:29 -0400

    Working on splashscreen

---
+++ app/splashscreen.jsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native'
+
+const splashscreen = () => {
+  return (
+    <View>
+      <Text>splashscreen</Text>
+    </View>
+  )
+}
+
+export default splashscreen
+
+const styles = StyleSheet.create({})
